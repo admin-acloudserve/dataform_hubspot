6fbafe3589a0134ec1c19281422ddf1cec1371b0
ADMIN ADMIN <admin@acloudserve.com>
2023-03-07 08:31:36 +0000

    Test dependencies.js

--- includes/constants.js
+++ includes/constants.js
@@ -1,8 +1,8 @@
 const SOURCE_DATASET = "hubspot_copy";
 const SOURCE_PROJECT = "sample1-328718";
 
-const email_metrics = ['bounces', 'clicks', 'deferrals', 'deliveries', 'drops', 'forwards', 'opens', 'prints', 'spam_reports', 'unsubscribes'],
-const hubspot_contact_merge_audit_enabled = false
+const email_metrics = ['bounces', 'clicks', 'deferrals', 'deliveries', 'drops', 'forwards', 'opens', 'prints', 'spam_reports', 'unsubscribes'];
+const hubspot_contact_merge_audit_enabled = false;
 
 
 
